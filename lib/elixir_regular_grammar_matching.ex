128c74e782d56094a33ee403ce1465b29e0eef70
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-02-26 15:52:15 -0300

    Add grammar rule application

--- lib/elixir_regular_grammar_matching.ex
+++ lib/elixir_regular_grammar_matching.ex
@@ -1,16 +1,26 @@
 defmodule ElixirRegularGrammarMatching do
   def apply_rules(nonterminals, terminals, rules, sentence) do
-    []
+    search_nonterminal(sentence, nonterminals) |>
+      Enum.flat_map(fn(i) ->
+        nonterminal = String.at(sentence, i)
+        Enum.reduce(rules, [], fn({condition, rule}, acc) ->
+          if condition == nonterminal do
+            [String.slice(sentence, 0..i - 1) <> rule <> String.slice(sentence, i + 1..-1) | acc]
+          else
+            acc
+          end
+        end)
+      end)
   end
 
   def search_nonterminal(sentence, nonterminals) do
     Enum.zip(to_charlist(sentence), 0..String.length(sentence)) |>
-    Enum.reduce([], fn({x, index}, acc) ->
-      if nonterminals =~ <<x>> do
-        [index | acc]
-      else
-        acc
-      end
-    end)
+      Enum.reduce([], fn({x, index}, acc) ->
+        if nonterminals =~ <<x>> do
+          [index | acc]
+        else
+          acc
+        end
+      end)
   end
 end
--- test/elixir_regular_grammar_matching_test.exs
+++ test/elixir_regular_grammar_matching_test.exs
@@ -5,4 +5,10 @@ defmodule ElixirRegularGrammarMatchingTest do
   test "searches for nonterminals" do
     assert ElixirRegularGrammarMatching.search_nonterminal("AvOCaDo", "AOCD") == [5, 3, 2, 0]
   end
+
+  test "applies an iteration of rules" do
+    assert ElixirRegularGrammarMatching.apply_rules("AB", "ab",
+      [{"A", "ABa"}, {"A", "b"}, {"B", "Bb"}, {"B", "b"}], "aABa") ==
+      ["aAba", "aABba", "abBa", "aABaBa"]
+  end
 end
