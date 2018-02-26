2b6cde5c31baac3b4449c24b2162a6540af71016
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-02-26 15:21:17 -0300

    Add nonterminal symbol search

--- lib/elixir_regular_grammar_matching.ex
+++ lib/elixir_regular_grammar_matching.ex
@@ -1,2 +1,16 @@
 defmodule ElixirRegularGrammarMatching do
+  def apply_rules(nonterminals, terminals, rules, sentence) do
+    []
+  end
+
+  def search_nonterminal(sentence, nonterminals) do
+    Enum.zip(to_charlist(sentence), 0..String.length(sentence)) |>
+    Enum.reduce([], fn({x, index}, acc) ->
+      if nonterminals =~ <<x>> do
+        [index | acc]
+      else
+        acc
+      end
+    end)
+  end
 end
--- test/elixir_regular_grammar_matching_test.exs
+++ test/elixir_regular_grammar_matching_test.exs
@@ -1,4 +1,8 @@
 defmodule ElixirRegularGrammarMatchingTest do
   use ExUnit.Case
   doctest ElixirRegularGrammarMatching
+
+  test "searches for nonterminals" do
+    assert ElixirRegularGrammarMatching.search_nonterminal("AvOCaDo", "AOCD") == [5, 3, 2, 0]
+  end
 end
