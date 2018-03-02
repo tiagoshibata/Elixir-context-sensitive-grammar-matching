193abedbceea18b86d39ace4b927e32562ba4a10
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-03-02 14:49:18 -0300

    Add is_terminal test to sentences

--- lib/elixir_regular_grammar_matching.ex
+++ lib/elixir_regular_grammar_matching.ex
@@ -1,5 +1,16 @@
 defmodule ElixirRegularGrammarMatching do
-  def apply_rules(nonterminals, terminals, rules, sentence) do
+  def search_nonterminal(sentence, nonterminals) do
+    Enum.zip(to_charlist(sentence), 0..String.length(sentence)) |>
+      Enum.reduce([], fn({x, index}, acc) ->
+        if nonterminals =~ <<x>> do
+          [index | acc]
+        else
+          acc
+        end
+      end)
+  end
+
+  def apply_rules(nonterminals, rules, sentence) do
     search_nonterminal(sentence, nonterminals) |>
       Enum.flat_map(fn(i) ->
         nonterminal = String.at(sentence, i)
@@ -13,14 +24,7 @@ defmodule ElixirRegularGrammarMatching do
       end)
   end
 
-  def search_nonterminal(sentence, nonterminals) do
-    Enum.zip(to_charlist(sentence), 0..String.length(sentence)) |>
-      Enum.reduce([], fn({x, index}, acc) ->
-        if nonterminals =~ <<x>> do
-          [index | acc]
-        else
-          acc
-        end
-      end)
+  def is_terminal(terminals, sentence) do
+    Enum.all?(to_charlist(sentence), &(not (terminals =~ <<&1>>)))
   end
 end
--- test/elixir_regular_grammar_matching_test.exs
+++ test/elixir_regular_grammar_matching_test.exs
@@ -7,8 +7,16 @@ defmodule ElixirRegularGrammarMatchingTest do
   end
 
   test "applies an iteration of rules" do
-    assert ElixirRegularGrammarMatching.apply_rules("AB", "ab",
+    assert ElixirRegularGrammarMatching.apply_rules("AB",
       [{"A", "ABa"}, {"A", "b"}, {"B", "Bb"}, {"B", "b"}], "aABa") ==
       ["aAba", "aABba", "abBa", "aABaBa"]
+  end
+
+  test "checks whether a sentence has exclusively terminal characters" do
+    assert ElixirRegularGrammarMatching.is_terminal("AB", "ab")
+    assert ElixirRegularGrammarMatching.is_terminal("A", "")
+    assert ElixirRegularGrammarMatching.is_terminal("", "ab")
+    assert not ElixirRegularGrammarMatching.is_terminal("AB", "A")
+    assert not ElixirRegularGrammarMatching.is_terminal("A", "aAb")
   end
 end
