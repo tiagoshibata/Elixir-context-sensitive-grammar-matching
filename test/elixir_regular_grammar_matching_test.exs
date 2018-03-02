558ff5d6416028c19aad82b252ee7a634328a6d3
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-03-02 16:03:31 -0300

    Add context sensitive grammar tests

--- test/elixir_regular_grammar_matching_test.exs
+++ test/elixir_regular_grammar_matching_test.exs
@@ -40,5 +40,17 @@ defmodule ElixirRegularGrammarMatchingTest do
     assert ElixirRegularGrammarMatching.can_generate_sentence(grammar, "a")
     assert ElixirRegularGrammarMatching.can_generate_sentence(grammar, "aba")
     assert ElixirRegularGrammarMatching.can_generate_sentence(grammar, "abbabbba")
+    assert not ElixirRegularGrammarMatching.can_generate_sentence(grammar, "ba")
+    assert not ElixirRegularGrammarMatching.can_generate_sentence(grammar, "abab")
+    context_sensitive_grammar = {
+      "AB",
+      "ab",
+      [{"A", "aAa"}, {"aAa", "BaB"}, {"B", "Bb"}, {"B", "b"}],
+      "A"
+    }
+    assert ElixirRegularGrammarMatching.can_generate_sentence(context_sensitive_grammar, "bab")
+    assert ElixirRegularGrammarMatching.can_generate_sentence(context_sensitive_grammar, "aabbbabaa")
+    assert ElixirRegularGrammarMatching.can_generate_sentence(context_sensitive_grammar, "aaabbbbbabbaaa")
+    assert not ElixirRegularGrammarMatching.can_generate_sentence(context_sensitive_grammar, "aabaaa")
   end
 end
